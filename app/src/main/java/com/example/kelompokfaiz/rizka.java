820edcf26ab2c996ea76e8cad4dcfd9d0998c509
alrizkafrizia <alrizkafrizia02@gmail.com>
2021-09-21 23:08:02 +0700

    splash screen

--- app/src/main/java/com/example/kelompokfaiz/rizka.java
+++ app/src/main/java/com/example/kelompokfaiz/rizka.java
@@ -1,5 +1,7 @@
 package com.example.kelompokfaiz;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class rizka extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rizka);
+        int SPLASH_SCREEN_TIME = 5000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(rizka.this, MainActivity.class);
+                rizka.this.startActivity(intent);
+                rizka.this.finish();
+            }
+        }, SPLASH_SCREEN_TIME);
     }
 }
