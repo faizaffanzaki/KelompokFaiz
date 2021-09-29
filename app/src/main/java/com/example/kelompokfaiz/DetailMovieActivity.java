10c146ea544f25fd84f599bed6101d1c505f6c2f
faizaffanzaki <faizzaki05@gmail.com>
2021-09-29 14:16:48 +0700

    Perubahan Faiz

---
+++ app/src/main/java/com/example/kelompokfaiz/DetailMovieActivity.java
@@ -0,0 +1,14 @@
+package com.example.kelompokfaiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_movie);
+    }
+}
