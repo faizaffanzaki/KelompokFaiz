4d70fcd7d8186eb70a66651920a9ff9239a3af48
alrizkafrizia <alrizkafrizia02@gmail.com>
2021-09-29 13:09:12 +0700

    TAMBAHAN LIBRARY baru

--- app/src/main/java/com/example/kelompokfaiz/rizka.java
+++ app/src/main/java/com/example/kelompokfaiz/rizka.java
@@ -2,7 +2,7 @@ package com.example.kelompokfaiz;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class rizka extends AppCompatActivity {
